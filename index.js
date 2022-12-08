8f8c33470f6a091540ea04c909ed253e7db5dc5b
Bilal Hassan <bilalhassain14@gmail.com>
2022-12-09 00:58:18 +0500

    Database fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose")
 const cors = require("cors");
 const app = express();
 
@@ -7,22 +8,20 @@ app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 1111;
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.DB_CONNECTION);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-};
+const connectDB = mongoose
+  .connect(
+    process.env.DB_CONNECTION,
+    { useUnifiedTopology: true, useNewUrlParser: true }
+  )
+  .then(() => console.log("Connected to database successfully"))
+  .catch((error) => console.log(error.message));
 
 const studentRoutes = require("./routes/student");
 app.use(studentRoutes);
 
 //Connecting to server
-connectDB().then(() => {
+if (connectDB){
   app.listen(PORT, () => {
     console.log(`Server is Running at http://localhost:${PORT}`);
   });
-});
+}
