9f448317b7bb1739059a8d529414f931e6473f99
Bilal Hassan <bilalhassain14@gmail.com>
2023-02-15 01:08:32 +0500

    bugs fixes

--- db.js
+++ db.js
@@ -2,14 +2,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 module.exports = mongoose
-  .connect(
-    "mongodb://localhost:27017/studentReg",
-    { useUnifiedTopology: true, useNewUrlParser: true }
-  )
+  .connect(process.env.DB_CONNECTION, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
   .then(() => console.log("Connected to database successfully"))
   .catch((error) => console.log(error.message));
-
-  // .connect(
-  //   process.env.DB_CONNECTION,
-  //   { useUnifiedTopology: true, useNewUrlParser: true }
-  // )
